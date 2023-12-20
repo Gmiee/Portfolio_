afad14d406fe5a67be79cd3758ceab817df365e7
Khemnani Jenish <104885605+Gmiee@users.noreply.github.com>
2023-12-20 12:33:11 +0530

    vercel speed insight and analy..

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 import { ChakraProvider } from '@chakra-ui/react';
 import Header from '@/Components/Header';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Poppins({ subsets: ['latin'],  weight : '300'})
 export const metadata = {
@@ -16,6 +18,8 @@ export default function RootLayout({ children }) {
         <ChakraProvider>
         <Header/>
         {children}
+        <SpeedInsights />
+        <Analytics />
         </ChakraProvider>        
         </body>
     </html>
