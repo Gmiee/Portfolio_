9ad169ac009adfa1a8b3c07a3e5cdd40b83dce31
Khemnani Jenish <104885605+Gmiee@users.noreply.github.com>
2024-01-16 17:11:48 +0530

    ...

--- Components/Carousel.jsx
+++ Components/Carousel.jsx
@@ -12,36 +12,40 @@ const Carousel = () => {
 
     return (
         <>
-            <Swiper
-                direction={'horizontal'}
-                slidesPerView={1}
-                spaceBetween={30}
-                mousewheel={true}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Mousewheel, Pagination]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <img src="/06.jpg" alt="img1" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/04.jpg" alt="img2" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/05.jpg" alt="img3" />
-                </SwiperSlide>
-            </Swiper>
-            <div className="txt flex flex-wrap p-2 items-center">
-                We are currently conducting a humble initiative to provide meals for those in need.
-                <button className='bg-surface rounded-lg text-bp px-4 py-2 hover:bg-transparent dark:bg-black dark:text-white dark:hover:bg-blk flex justify-center gap-1 capitalize' >
-                    <a href='https://annapurnaa.in/' target='_blank'>
-                    Join us now
-                    </a>
-                    <CiLocationArrow1 style={{ fontSize: '1.3rem' }} />
-                </button>
+            <div className='flex max-sm:grid'> 
+                <Swiper
+                    direction={'horizontal'}
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    mousewheel={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Mousewheel, Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src="/06.jpg" alt="img1" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/04.jpg" alt="img2" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/05.jpg" alt="img3" />
+                    </SwiperSlide>
 
+                </Swiper>
+
+                <div className="txt flex flex-wrap p-2 items-center">
+                    We are currently conducting a humble initiative to provide meals for those in need.
+                    <button className='bg-surface rounded-lg text-bp px-4 py-2 hover:bg-transparent dark:bg-black dark:text-white dark:hover:bg-blk flex justify-center gap-1 capitalize' >
+                        <a href='https://annapurnaa.in/' target='_blank'>
+                            Join us now
+                        </a>
+                        <CiLocationArrow1 style={{ fontSize: '1.3rem' }} />
+                    </button>
+
+                </div>
             </div>
         </>
     )
