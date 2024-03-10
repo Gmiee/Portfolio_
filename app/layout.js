af12079467cfbef45326ab86cf93f1d72bd792b1
Khemnani Jenish <104885605+Gmiee@users.noreply.github.com>
2024-03-11 00:05:16 +0530

    font family

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,33 @@
-import './globals.css'
-import { Rubik } from 'next/font/google'
-import { ChakraProvider } from '@chakra-ui/react';
-import Header from '@/Components/Header';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from '@vercel/analytics/react';
-import Provider from '@/Components/Provider';
+import "./globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import Provider from "@/Components/Provider";
+import localFont from "next/font/local";
 // import ThemeSwitcher from './ThemeSwitcher';
-const inter = Rubik({ subsets: ['latin'],  weight : '300'})
+
+const myFont = localFont({
+  subsets: ["latin"],
+  src: "./fonts/Satoshi-Regular.ttf",
+});
 export const metadata = {
   title: "Jenish Khemnani's Portfolio!  ",
-  description: 'On a road to become MERN stack developer.',
-}
+  description: "On a road to become MERN stack developer.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body style={myFont.style} className={myFont.className}>
         <ChakraProvider>
-        <Provider>
-        {/* <ThemeSwitcher /> */}
-        {children}
-        </Provider>
-        <SpeedInsights />
-        <Analytics />
-        </ChakraProvider>        
-        </body>
+          <Provider>
+            {/* <ThemeSwitcher /> */}
+            {children}
+          </Provider>
+          <SpeedInsights />
+          <Analytics />
+        </ChakraProvider>
+      </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors:{
-        surface:'#f6f6f7',
-        bp:"#222222",
-        gray5:'#c7c7c7',
-        gray8:'#909090',
-        gray9:'#e9e9e9',
-        avail:'#4FD64C',
-        surface2:'#0A0A0A',
-        blk:'#212121',
-        color:'#fff'
-      },
-      fontFamily: {
-        sans: ['sans','serif'],
-        rubik:['Rubik'],
-        serif: ['Georgia', 'serif'],
+      colors: {
+        surface: "#f6f6f7",
+        bp: "#222222",
+        gray5: "#c7c7c7",
+        gray8: "#909090",
+        gray9: "#e9e9e9",
+        avail: "#4FD64C",
+        surface2: "#0A0A0A",
+        blk: "#212121",
+        color: "#fff",
       },
       fontWeight: {
         thin: 100,
@@ -29,5 +24,5 @@ module.exports = {
     },
   },
   plugins: [],
-  darkMode:'class'
-}
+  darkMode: "class",
+};
